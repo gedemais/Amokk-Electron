05c0b156cdd5ea03071ba22167e9c5987cc47892
LeMeubleRandom <antoinedbc2005@gmail.com>
2026-02-19 13:23:13 +0100

    correction bouton

--- src/components/dashboard/PlanCard.tsx
+++ src/components/dashboard/PlanCard.tsx
@@ -4,7 +4,6 @@ import { url } from "inspector/promises";
 import { Check } from "lucide-react";
 import { useTranslation } from "react-i18next";
 
-
 interface PlanCardProps {
   title: string;
   price: string;
@@ -19,20 +18,32 @@ interface PlanCardProps {
   link: string;
 }
 
-const PlanCard = ({ title, price, period, description, features, onSelect, isUpgrade, showButton = true, isCurrentPlan, planId, link }: PlanCardProps) => {
+const PlanCard = ({
+  title,
+  price,
+  period,
+  description,
+  features,
+  onSelect,
+  isUpgrade,
+  showButton = true,
+  isCurrentPlan,
+  planId,
+  link,
+}: PlanCardProps) => {
   const { t } = useTranslation();
   const handle_select = () => {
     const links = {
-      3: 'https://amokkcoaching.lemonsqueezy.com/checkout/buy/812bef86-3ada-4a98-9fe8-05518f3cec4e', // Starter LemonSqueezy link
-      4: 'https://amokkcoaching.lemonsqueezy.com/checkout/buy/6155b329-917a-41f3-a77d-fdb17a72cac7', // Try-Hard LemonSqueezy link
-      5: 'https://amokkcoaching.lemonsqueezy.com/checkout/buy/fd35a47d-f594-4c39-ae32-34aac151d923', // Rush LemonSqueezy link
-    }
+      3: "https://amokkcoaching.lemonsqueezy.com/checkout/buy/812bef86-3ada-4a98-9fe8-05518f3cec4e", // Starter LemonSqueezy link
+      4: "https://amokkcoaching.lemonsqueezy.com/checkout/buy/6155b329-917a-41f3-a77d-fdb17a72cac7", // Try-Hard LemonSqueezy link
+      5: "https://amokkcoaching.lemonsqueezy.com/checkout/buy/fd35a47d-f594-4c39-ae32-34aac151d923", // Rush LemonSqueezy link
+    };
 
     const plans_names = {
-      3: 'starter',
-      4: 'try_hard',
-      5: 'rush',
-    }
+      3: "starter",
+      4: "try_hard",
+      5: "rush",
+    };
 
     const userEmail = localStorage.getItem("user_email");
 
@@ -44,21 +55,21 @@ const PlanCard = ({ title, price, period, description, features, onSelect, isUpg
 
     url.searchParams.set("checkout[custom][plan_name]", plans_names[planId]);
 
-    window.open(url.toString(), '_blank', 'noopener,noreferrer');
+    window.open(url.toString(), "_blank", "noopener,noreferrer");
   };
   return (
     <Card className="border-border/50 hover:border-primary/50 transition-all cursor-pointer group relative overflow-hidden">
       {/* Upgrade badge */}
       {isUpgrade && (
         <div className="absolute top-0 right-0 bg-gradient-to-br from-primary to-primary text-white text-xs font-bold px-4 py-1 rounded-bl-lg">
-          {t('components.dashboard.PlanCard.upgrade_badge')}
+          {t("components.dashboard.PlanCard.upgrade_badge")}
         </div>
       )}
 
       {/* Current plan badge */}
       {isCurrentPlan && (
         <div className="absolute top-0 left-0 bg-green-500 text-white text-xs font-bold px-4 py-1 rounded-br-lg z-20">
-          {t('components.dashboard.PlanCard.current_plan_badge')}
+          {t("components.dashboard.PlanCard.current_plan_badge")}
         </div>
       )}
 
@@ -80,12 +91,12 @@ const PlanCard = ({ title, price, period, description, features, onSelect, isUpg
               <span className="text-muted-foreground">{period}</span>
             </div>
             <p className="text-xl font-bold text-muted-foreground mt-2">
-              {t('components.dashboard.PlanCard.no_commitment')}
+              {t("components.dashboard.PlanCard.no_commitment")}
             </p>
             <p className="text-sm text-muted-foreground mt-3">{description}</p>
           </div>
 
-          <div className="space-y-3 text-left">
+          <div className="space-y-3 text-left grow flex-grow 1">
             {features.map((feature, index) => (
               <div key={index} className="flex gap-3 items-start">
                 <Check className="h-5 w-5 text-primary flex-shrink-0 mt-0.5" />
@@ -95,14 +106,17 @@ const PlanCard = ({ title, price, period, description, features, onSelect, isUpg
           </div>
 
           {showButton && (
-            <Button className="w-full self-end" variant="outline" onClick={handle_select}>
-              {t('components.dashboard.PlanCard.start_btn')}
+            <Button
+              className="w-full self-end"
+              variant="outline"
+              onClick={handle_select}
+            >
+              {t("components.dashboard.PlanCard.start_btn")}
             </Button>
           )}
         </div>
       </CardContent>
     </Card>
-
   );
 };
 
